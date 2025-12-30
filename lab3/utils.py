b8baa8a288bf5fe92e7ef19717926cffde8fc740
EgorChernov5 <krish19poroh@mail.ru>
2025-12-30 16:37:55 +0300

    Fix batch_size for test

--- lab3/utils.py
+++ lab3/utils.py
@@ -88,7 +88,7 @@ def get_dataloaders(
     )
     test_loader = DataLoader(
         test_ds,
-        batch_size=batch_size,
+        batch_size=5,
         shuffle=False,
         num_workers=4
     )
